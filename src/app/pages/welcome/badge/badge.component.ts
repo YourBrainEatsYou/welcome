3177cf1864935fd3c033d171d27428bb4556a9e7
YourBrainEatsYou <me@yourbraineatsyou.ch>
2022-03-02 22:05:40 +0100

    fixed NFC

--- src/app/pages/welcome/badge/badge.component.ts
+++ src/app/pages/welcome/badge/badge.component.ts
@@ -9,11 +9,6 @@ export class BadgeComponent implements OnInit {
 
   @Output() badgeSerialNumber: EventEmitter<string> = new EventEmitter<string>();
 
-  serialNumber = '';
-
-  constructor() {
-  }
-
   ngOnInit(): void {
     const scan = async () => {
       if ("NDEFReader" in window) {
@@ -24,36 +19,18 @@ export class BadgeComponent implements OnInit {
           ndef.addEventListener("readingerror", () => {
             alert("Argh! Cannot read data from the NFC tag. Try another one?");
           });
-          
+
           // @ts-ignore
           ndef.addEventListener("reading", ({serialNumber}) => {
-            this.serialNumber = serialNumber;
             this.badgeSerialNumber.emit(serialNumber);
           });
         } catch (error) {
           console.log("Argh! " + error);
         }
-        /*
-                ndef.scan().then(() => {
-
-                  ndef.onreading = (event: NDEFReadingEvent) => {
-                    alert(event.serialNumber);
-                    this.serialNumber = event.serialNumber;
-                    // this.badgeSerialNumber.emit(event.serialNumber);
-                  };
-                }).catch(() => {
-                });
-
-         */
       } else {
         console.log('NDEF not suported');
       }
     };
     scan();
   }
-
-  badgeScanned() {
-    this.badgeSerialNumber.emit(this.serialNumber);
-  }
-
 }
